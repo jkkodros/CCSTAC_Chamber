edc1023408516364a72f4d8c8854054cb21c98f0
J Kodros <jkkodros.research@gmail.com>
2020-05-26 12:40:14 +0300

    Fixed plotting routine. Messy work with the arrays though

--- spectra.py
+++ spectra.py
@@ -14,20 +14,21 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
 class Spectra:
-    def __init__(self, mz, data):
+    def __init__(self, mz, data, relTime=None):
         self.mz = mz
         self.data = data
+        self.relTime = relTime
         
     def convert_to_umr(self):
-        if len(self.data) > 1:
+        if type(self.relTime) == np.ndarray:
             timeSeries = True
         else:
             timeSeries = False
         self.mz_umr = np.arange(12, int(max(self.mz))+1, 1)
         if timeSeries:
-            self.data_umr = np.zeros((len(self.data),len(self.mz)))
+            self.data_umr = np.zeros((len(self.data),len(self.mz_umr)))
         else:
-            self.data_umr = np.zeros(len(self.mz))
+            self.data_umr = np.zeros(len(self.mz_umr))
     
         for i in range(0, len(self.mz_umr)):
             ix1 = np.where(self.mz >= self.mz_umr[i] - 0.5 )[0][0]
@@ -36,18 +37,31 @@ class Spectra:
                 self.data_umr[:,i] = self.data[:,ix1:ix2+1].sum(1)
             else:
                 self.data_umr[i] = self.data[ix1:ix2+1].sum()
+                
+    def normalize_spectra(self):
+        if type(self.relTime) == np.ndarray:
+            self.data_umr_normalized = np.zeros(np.shape(self.data_umr))
+            for i in range(0, len(self.data_umr)):
+                self.data_umr_normalized[i,:] = (self.data_umr[i,:]
+                                                 /self.data_umr[i,:].sum())
 
-    def plot_spectra(self, time_period, UMR=True, spectra2=None):
-        if len(self.data_umr) > 1:
-            spectra = self.data_umr.mean(axis=0)
+    def plot_spectra(self, time_period=[[-0.5, 0.0]], UMR=True, 
+                     normalized=True):
+        if type(self.relTime) == np.ndarray:
+            spectras = []
+            for period in time_period:
+                spectra = self.data_umr[(self.relTime >= period[0]) & (
+                    self.relTime < period[1]), :].mean(axis=0)
+                if normalized:
+                    spectra = spectra / spectra.sum()
+                spectras.append(spectra)
         else:
             spectra = self.data_umr
             
         fig, ax = plt.subplots(figsize=(10,5))
-        ax.bar(self.mz, spectra, color='C2')
-        if spectra2:
-            ax.plot(spectra2.mz, spectra2.data, 'o', color='k')
-        
+        if len(spectra) > 1:
+            ax.bar(self.mz_umr, spectras[0], color='C2')
+            ax.plot(self.mz_umr, spectras[1], 'o', color='k')
         ax.set_xlabel('m/z', fontsize=14)
         ax.set_ylabel('Normalized Spectra', fontsize=14)
         ax.set_ylim(0.0)
